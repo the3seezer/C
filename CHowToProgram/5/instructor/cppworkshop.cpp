1b4ca29839c40964461887d3fdfc613836fb570e
the3seezer <the3seezer@gmail.com>
2023-05-11 17:50:31 +0300

    5:50 PM 5/11/2023

---
+++ CHowToProgram/5/instructor/cppworkshop.cpp
@@ -0,0 +1,16 @@
+/*
+ theBitRiddler
+ c++
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool ismember = true;
+    cout << ismember << endl;
+    ismember = false;
+    string characters = "a283oka";
+    cout << characters << ismember;
+    return 0;
+}
