55e8b4e3646afcfc6d932946d16b35bd7dc28eb9
the3seezer <the3seezer@gmail.com>
2023-07-03 12:48:38 +0300

    fig7.12 workshop

--- CHowToProgram/7/fig7.12workshop.c
+++ CHowToProgram/7/fig7.12workshop.c
@@ -1,3 +1,17 @@
 /*
 theBitRiddler
+7/3/2023
+12:29 PM
+Finding the number of elements of an Array using the sizeof operator
 */
+#include <stdio.h>
+
+#define SIZE 22 
+
+int main( void ) {
+    double real[ SIZE ] = { 0 };
+
+    printf_s( "The number of elements of real is %zu\n", sizeof( real ) / sizeof( real[ 0 ] ) );
+
+    return 0;
+} /* end main */
