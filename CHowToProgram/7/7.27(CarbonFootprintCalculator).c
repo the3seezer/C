06661420925fee7addbbf0dffcc9aeff2023b817
the3seezer <the3seezer@gmail.com>
2023-10-09 21:43:36 +0300

    9:43 PM 10/9/2023

--- CHowToProgram/7/7.27(CarbonFootprintCalculator).c
+++ CHowToProgram/7/7.27(CarbonFootprintCalculator).c
@@ -6,6 +6,72 @@ theBitRiddler
 */
 #include <stdio.h>
 
+void car( void );
+void bicycle( void );
+void building( void );
+void printMenu( void );
+
 int main(void){
+    int choice = 0;
+    void ( * choose[ 3 ] ) ( int ) = { car, bicycle, building };
+
+    while( choice >= 0 && choice < 4 ) {
+        do {
+            printMenu();
+            scanf( "%d", &choice );
+        }
+        while( choice < -1 || choice > 2 );
+		
+		if ( choice >= 0 )
+        	( * choose[ choice ] ) ( choice );
+        else
+        	break;
+        	
+    } // end while
+    return 0;
 
 } /* end main */
+
+void printMenu( void ) {
+    printf_s( "%s", "\tTake a choice to calculate"
+                    " carbon footprint\n"
+                    "\t 0. Car\n"
+                    "\t 1. Bicycle\n"
+                    "\t 2. Building\n"
+                    "\t -1 to end \n"
+                    "\t ? " );
+} /* end function printMenu */
+
+void car( void ) {
+    int driving = 0;
+    int asset = 0; // miles
+
+    printf_s( "%s", "\tChoose 0 not driving and 1 when driving\n"
+                    "\tAnd Choose how many miles ");
+    scanf( "%d%d", &driving, &asset );
+
+    if ( driving ) {
+        printf_s( "\tCarbon emission grams %d\n", 280 * asset );
+    } // end if
+    else {
+        printf_s( "\tCarbon emission grams %d\n", 65 * asset );
+    } // end else
+
+} /* end function car */
+
+void bicycle( void ) {
+    int mile = 0;
+    printf_s( "%s", "\tChoose how many miles " );
+    scanf( "%d", &mile );
+    printf_s( "\tCarbon emission grams %d\n", 33 * mile );
+} /* end function bicycle */
+
+void building( void ) {
+    int sqMeters = 0;
+    int structuralElement = 0;
+    printf_s( "%s", "\tchoose how many square meters " );
+    scanf( "%d", &sqMeters );
+    printf_s( "%s", "\tChoose how many structural element square meters " );
+    scanf( "%d", &structuralElement );
+    printf_s( "\tCarbon emission in gram per year %d\n", 48870 * sqMeters + 6940 * structuralElement );
+} /* end function building */
---
+++ CHowToProgram/7/7.28(Machine-LanguageProgramming).c
@@ -0,0 +1,26 @@
+/*
+theBitRiddler
+10/6/2023
+10:50 AM
+ (Machine-Language Programming) 
+*/
+#include <stdio.h>
+// Input/Output operations:
+#define READ 10 // Read a word from the keyboard into a specific location in memory.
+#define WRITE 11 // Write a word from a specific location in memory to the screen.
+// Load/store operations:
+#define LOAD 20 // Load a word from a specific location in memory into the accumulator.
+#define STORE 21 // Store a word from the accumulator into a specific location in memory.
+// Arithmetic operations:
+#define ADD 30 // Add a word from a specific location in memory to the word in the accumulator ( leave the result in the accumulator).
+#define SUBTRACT 31 // Substract a word from a specific location in memory into the word in the accumulator ( leave the result in the accumulator).
+#define DIVIDE 32 // Divide a word from a specific location in memory into the word in the accumulator (leave the result in the accumulator).
+#define MULTIPLY 33 // Multiply a word from a specific location in memory by the word in the accumulator (leave the result in the accumulator).
+// transfer-of-control operations:
+#define BRANCH 33 // Branch to a specific location in a memory.
+#define BRANCHNEG 41 // Branch to a specific location in memory if the accumulator is negative.
+#define BRANCHZERO 42 // Branch to a specific location in memory if the accumulator is zero.
+#define HALT 43 // Halt---i.e., the program has completed its task.
+int main( void ) {
+    
+} /* end main */
