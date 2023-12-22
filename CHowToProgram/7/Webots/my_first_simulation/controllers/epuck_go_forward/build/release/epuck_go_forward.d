b5f3fb6c58d666aaed84f5908067966495275212
the3seezer <the3seezer@gmail.com>
2023-12-22 21:57:15 +0300

    webots

---
+++ CHowToProgram/7/Webots/my_first_simulation/controllers/epuck_go_forward/build/release/epuck_go_forward.d
@@ -0,0 +1,5 @@
+build/release/epuck_go_forward.o: epuck_go_forward.c \
+ C:\Users\abudu\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\abudu\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\abudu\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\abudu\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h
