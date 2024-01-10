c86616289faff067057872421e77fdd57842f17f
the3seezer <the3seezer@gmail.com>
2024-01-10 14:07:11 +0300

    moved them to their repositories

--- CHowToProgram/7/Webots/my_first_simulation/controllers/epuck_avoid_collision/build/release/epuck_avoid_collision.d
+++
@@ -1,6 +0,0 @@
-build/release/epuck_avoid_collision.o: epuck_avoid_collision.c \
- C:\Users\abudu\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
- C:\Users\abudu\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\abudu\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
- C:\Users\abudu\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\abudu\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h
